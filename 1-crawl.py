9d410091170f8017caef7f45301fe36689651d00
David Gengenbach <davidgengenbach@gmx.de>
2017-04-18 17:03:51 +0200

    :)

--- 1-crawl.py
+++ 1-crawl.py
@@ -4,23 +4,32 @@ from attrdict import AttrDict
 import mechanicalsoup
 import json
 
-PRICE = 'preis:0:20'
-URL = 'https://www.ebay-kleinanzeigen.de/s-64283/{}/seite:{}/l4896'.format(PRICE, '{}')
 JSON_OUT = 'data/results.json'
 
-
 def main():
+    args = get_args()
     browser = mechanicalsoup.Browser()
-    page_num = 1
-    page_max = 10
     results = []
-    for i in range(page_num, page_max):
-        results += get_results(browser, URL, i)
-    with open(JSON_OUT, 'w') as f:
+    for page_num in range(args.page_start, args.page_end):
+        results += get_results(browser, args.url, page_num)
+    with open(args.json_out, 'w') as f:
         json.dump(results, f, indent = 4, sort_keys = True)
 
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Crawl ebay kleinanzeigen')
+    parser.add_argument('--url', default = 'https://www.ebay-kleinanzeigen.de/s-64283/%s/l4896', help = 'The start url. Must have a "%s" portion in the url to insert the "options" like the page num, price etc.')
+    parser.add_argument('--page-start', default = 1, help = 'The page number to start at')
+    parser.add_argument('--page-end', default = 10, help = 'The page number to end at')
+    parser.add_argument('--json-out', default = JSON_OUT, help = 'The path for the output json.')
+    parser.add_argument('--options', default = 'preis:0:20/', help = 'Options for kleinanzeigen. Get from the site')
+    args = parser.parse_args()
+    args.url = args.url % (args.options + '/%s')
+    return args
+
+
 def get_results(browser, url, page):
-    page = browser.get(url.format(page))
+    page = browser.get(url % (page))
     # Dirty
     domain = '/'.join(url.split('/')[0:3])
     results = []
