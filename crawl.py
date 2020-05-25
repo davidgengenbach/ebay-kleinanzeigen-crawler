50c99a5ed210c067fc45099cbccaf6a2dcf853cb
David Gengenbach <info@davidgengenbach.de>
2020-05-25 15:33:58 +0200

    Options fix

--- crawl.py
+++ crawl.py
@@ -9,12 +9,15 @@ JSON_OUT = 'data/results.json'
 
 def main():
     args = get_args()
-    browser = mechanicalsoup.Browser()
+    browser = mechanicalsoup.Browser(
+        soup_config={'features': 'lxml'}
+    )
     results = []
     print("Starting to crawl. Options:\n{}\n".format(args))
     for page_num in range(args.page_start, args.page_end + 1):
-        print("\tCrawling page: {}/{}".format(page_num, args.page_end))
-        results += get_results(browser, args.url, page_num)
+        url = args.url % ('seite:' + str(page_num))
+        print("\tCrawling page: {:2}/{:2} ({})".format(page_num, args.page_end, url))
+        results += get_results(browser, args.url)
     with open(args.json_out, 'w') as f:
         json.dump(results, f, indent=4, sort_keys=True)
 
@@ -24,11 +27,11 @@ def main():
 def get_args():
     import argparse
     parser = argparse.ArgumentParser(description='Crawl ebay kleinanzeigen', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--url', default='https://www.ebay-kleinanzeigen.de/s-64283/%s/l4896', help='The start url. Must have a [percent-sign]s portion in the url to insert the "options" like the page num, price etc.')
+    parser.add_argument('--url', default='https://www.ebay-kleinanzeigen.de/s-saarbruecken/%s/peugeot/k0l382', help='The start url. Must have a [percent-sign]s portion in the url to insert the "options" like the page num, price etc.')
     parser.add_argument('--page-start', default=1, type=int, help='The page number to start at')
     parser.add_argument('--page-end', default=10, type=int, help='The page number to end at')
     parser.add_argument('--json-out', default=JSON_OUT, help='The path for the output json.')
-    parser.add_argument('--options', default='preis:0:20/', help='Options for kleinanzeigen. Get from the site')
+    parser.add_argument('--options', default='', help='Options for kleinanzeigen. Get from the site. Example: "--options preis:0:20"')
     args = parser.parse_args()
     args.url = args.url % (args.options + '/%s')
     return args
@@ -41,8 +44,8 @@ def render(ads):
         f.write(template.render(ads=ads))
 
 
-def get_results(browser, url, page):
-    page = browser.get(url % ('seite:' + str(page)))
+def get_results(browser, url):
+    page = browser.get(url)
     # Dirty
     domain = '/'.join(url.split('/')[0:3])
     results = []
